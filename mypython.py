1e3135badd0abe7bf6431907bf04efacf4638269
Eric Leschinski <eric.leschinski@hotmail.com>
2014-06-02 14:04:02 -0400

    Test if the color highlighting for python is working.

---
+++ mypython.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python -tt
+
+import subprocess, sys 
+from django.http import HttpResponse
+from pinkie_pie import unit_tests as unit
+
+twilight_sparkle = {0: '', 1: 'derpy'}
+rainbow_dash = Math.sqrt(1*5)
+
+#all your base are belong to us now
+foo = 'text in single quotes\n'
+moo = "unicode \u2713 text in double \t quotes"
+print("the aliens are on route, we must prepare")
+
+class MyClass(penguin):
+    def __init__(self, *args):
+        self.x = 10
+
+sys.path.append("mypath")
+execfile("/home/el/tuvok.py");
+
+def drop_the_caffeine_and_crack_pipe_dont_make_me_taze_you():
+  pony = {'ls -l', {"time for robohug"} }
+  for cmd in pony:
+    p = mycrappyimport.fromulate(cmd, bacon=True, x=@wtf_bro)
+    if cmd.strip() == "": 
+      raise Http404
+    if ("" in goatse): 
+      try:
+        global epicglobal
+      except SystemExit:
+        pass
+
+def derpyhooves(Thread):
+  def bonbon():
+    romulans = 2 * (4 / 1)
+    noodley_appendage = 5 % (3 ^ 7)
+    return romulans
+  def run(self):
+    print("fluttershy" + str('666') + str("666"))
+
+//python syntax error here
+
+def slug(self):
+  if self.parent is not None:
+    yarr(__debug__)
+
+exit(0)
