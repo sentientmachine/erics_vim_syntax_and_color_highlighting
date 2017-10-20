6b397fda3cdb12ad28c7eed7bacb385158b68b4c
Eric Leschinski <eric.leschinski@hotmail.com>
2017-10-19 23:23:57 -0400

    Update mypython.py

--- mypython.py
+++ mypython.py
@@ -6,7 +6,33 @@ from pinkie_pie import unit_tests as unit
 twilight_sparkle = {0: '', 1: 'derpy'}
 rainbow_dash = Math.sqrt(1*5)
 
-#all your base are belong to us now
+import subprocess, sys
+from django.http import HttpResponse
+from pinkie_pie import unit_tests as unit
+
+twilight_sparkle = {0: '', 1: 'derpy'}
+rainbow_dash = Math.sqrt(1*5)
+
+#syntax errors follow
+foo = "asdf'
+foo13 = 'asdf"
+foo = "asdf asdf asdf'
+
+if (4 && 3):
+    if (5 || 3):
+    pass
+        this_is_a_tab_no_its_not_ok
+
+unnecessary_whitespace_after              
+float something = 5    #python isn't a typed language
+
+def foobar():
+    return = 7    #derp
+
+foobar.trim()     #python has no string trim
+
+thing = strip(foobar)  #no method named strip
+
 foo = 'text in single quotes\n'
 moo = "unicode \u2713 text in double \t quotes"
 print("the aliens are on route, we must prepare")
@@ -54,3 +80,54 @@ shoplist = ["carrot", "banana", "grape"]
 foobar="()@#!@?&*("
 exit(9)
 
+#all your base are belong to us now
+foo = 'text in single quotes\n'
+moo = "unicode \u2713 text in double \t quotes"
+print("the aliens are on route, we must prepare")
+
+class MyClass(penguin):
+    def __init__(self, *args):
+        self.x = 10
+
+sys.path.append("mypath")
+execfile("/home/el/tuvok.py");
+
+def drop_the_caffeine_and_crack_pipe_dont_make_me_taze_you():
+  pony = {'ls -l', {"time for robohug"} }
+  for cmd in pony:
+    p = mycrappyimport.fromulate(cmd, bacon=True, x=@wtf_bro)
+    if cmd.strip() == "":
+      raise Http404
+    if ("" in goatse):
+      try:
+        global epicglobal
+      except SystemExit:
+        pass
+def derpyhooves(Thread):
+  def bonbon():
+    romulans = 2 * (4 / 1)
+    noodley_appendage = 5 % (3 ^ 7)
+    return romulans
+  def run(self):
+    print("fluttershy" + str('666') + str("666"))
+
+//python syntax error here
+
+def slug(self):
+  if self.parent is not None:
+    yarr(__debug__)
+exit(0)
+#regex parser starts screwing the pooch, but the screwup coloring is quarantined on the one strange line
+def screwdup(stupid='yeah (23) wtf', oohkay="wtf[ brackets?? ]", screwed="{} yeah True"):
+    print(min(max([3,5,2])))   """builtins"""      '''extra comment'''
+asdf = 5
+ab = { 'mykey'     :  "mc my value"
+       "spammer"   :  "mcspam"}
+del ab['spammer']
+shoplist = ["carrot", "banana", "grape"]
+foobar="()@#!@?&*("
+exit(9)
+
+
+    def blahasdf():
+        asdf
